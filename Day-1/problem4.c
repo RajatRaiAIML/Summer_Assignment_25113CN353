fb1358b6f34b6e2de8e749fd984e600e834091c5
Rajat Rai <rajatrai20160242@gmail.com>
2026-05-25 07:53:57 +0530

    Day-1 uploaded

---
+++ Day-1/problem4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,count=0;
+    printf("Enter a number: ");
+    while(n>0){
+        n=n/10;
+        count++;
+    }
+    printf("Number of digits=%d",count);
+    return 0;
+}
